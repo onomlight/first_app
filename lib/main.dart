1d8803bc4eff6feb3e1aba69899b5cf7a88da40e
onomlight <pass041@nate.com>
2023-05-29 16:52:50 +0900

    runApp 과 void 이유

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+  // runApp() 앱 실행 명령어
 }
+// void를 입력하고 사용자 지정 명령과 사용자 지정함수의 이름을 입력 ! 메인임
+// 그 후 void main(){} 함수 저장하는거 처럼 작성함
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
